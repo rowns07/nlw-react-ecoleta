e69d1456a79286f33a8857ca478a98c22e398b74
Diego Souza <diegosouza@bestpontos.com.br>
2020-06-06 20:31:10 -0300

    Adicionando upload de imagem com o dropzone

--- src/pages/CreatePoint/index.tsx
+++ src/pages/CreatePoint/index.tsx
@@ -7,6 +7,7 @@ import { Map, TileLayer, Marker } from 'react-leaflet';
 import api from '../../services/api';
 import Axios from 'axios';
 import { LeafletMouseEvent } from 'leaflet';
+import Dropzone from '../../components/dropzone';
 
 interface Item {
     id: number,
@@ -40,7 +41,10 @@ const CreatePoint = () => {
     const [selectedUf, setSelectedUf] = useState('0');
     const [selectedCity, setSelectedCity] = useState('0');
     const [selectedPosition, setSelectedPosition] = useState<[number, number]>([0, 0]);
+    const [selectedFile, setSelectedFile] = useState<File>();
+
     const history = useHistory();
+
     useEffect(() => {
         navigator.geolocation.getCurrentPosition(position => {
             const { latitude, longitude } = position.coords;
@@ -121,18 +125,22 @@ const CreatePoint = () => {
         const [latitude, longitude] = selectedPosition;
         const items = selectedItems;
 
-        const data = {
-            nome,
-            email,
-            whatsapp,
-            uf,
-            city,
-            latitude,
-            longitude,
-            items
+
+        const data = new FormData();
+
+        data.append('nome', nome)
+        data.append('email', email)
+        data.append('whatsapp', whatsapp)
+        data.append('uf', uf)
+        data.append('city', city)
+        data.append('latitude', String(latitude))
+        data.append('longitude', String(longitude))
+        data.append('items', items.join(','));
+
+        if (selectedFile) {
+            data.append('image', selectedFile)
         }
 
-        console.log(data);
         await api.post('points', data);
         alert('Ponto de coleta criado');
         history.push('/');
@@ -151,6 +159,7 @@ const CreatePoint = () => {
 
             <form onSubmit={handleSubmit}>
                 <h1>Cadastro do<br /> ponto de coleta</h1>
+                <Dropzone onFileUploaded={setSelectedFile} />
 
                 <fieldset>
                     <legend>
@@ -159,11 +168,11 @@ const CreatePoint = () => {
                     </h2>
                     </legend>
                     <div className="field">
-                        <label htmlFor="name">Nome da Entidade</label>
+                        <label htmlFor="nome">Nome da Entidade</label>
                         <input
                             type="text"
-                            name="name"
-                            id="name"
+                            name="nome"
+                            id="nome"
                             onChange={handleInputChange}
                         />
                     </div>
@@ -182,7 +191,7 @@ const CreatePoint = () => {
                             <label htmlFor="whatsapp">Whatsapp</label>
                             <input
                                 type="text"
-                                name="nawhatsappme"
+                                name="whatsapp"
                                 id="whatsapp"
                                 onChange={handleInputChange}
 
